28d48918c763930192df07df8e2a45afae02d102
Josh Mize <jgmize@gmail.com>
2015-03-05 13:11:06 -0600

    Add GITTERBOT_IRC_SERVER environment variable

--- bin.js
+++ bin.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
 var gitterBot = require('./')
 var opts = {
+  ircServer: process.env['GITTERBOT_IRC_SERVER'],
   ircChannel: process.env['GITTERBOT_IRC_CHANNEL'],
   ircNick: process.env['GITTERBOT_IRC_NICK'],
   gitterApiKey: process.env['GITTERBOT_APIKEY'],
   gitterRoom: process.env['GITTERBOT_GITTER_ROOM']
 }
 
-if(!(opts.ircChannel &&  opts.gitterApiKey && opts.gitterRoom && opts.ircNick)) {
+if(!(opts.ircServer && opts.ircChannel &&  opts.gitterApiKey && opts.gitterRoom && opts.ircNick)) {
   console.error('You need to set the config env variables (see readme.md)')
   process.exit()
 }
